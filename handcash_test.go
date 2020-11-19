23d2af50445a0daf309d9e197650416ccf3a9c91
Luke <rohenaz@gmail.com>
2020-11-18 19:12:09 -0500

    fix test

--- handcash_test.go
+++ handcash_test.go
@@ -10,10 +10,9 @@ func TestGetProfile(t *testing.T) {
 
 	t.Run("missing token", func(t *testing.T) {
 
-		profile, err := GetProfile("79f8dd768817ca86bcc06d468a67e64e4a349658c9d8af70e2251dfc2553de91")
+		_, err := GetProfile("")
 
-		// assert.Error(t, err)
-		t.Errorf("%s %s", profile, err)
+		assert.Error(t, err)
 	})
 
 	t.Run("bad token", func(t *testing.T) {
